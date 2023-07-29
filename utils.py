264425c7f0ce0c0b18109653a5d97abb01f57efc
Thimira Nirmal <timnirmal@gmail.com>
2023-07-29 16:22:24 +0530

    ndipjdsd

--- utils.py
+++ utils.py
@@ -9,23 +9,22 @@ import pandas as pd
 import streamlit
 import streamlit as st
 
-WS_CONN = "ws://localhost:8000/sample"
-async def consumer(graphs, selected_channels, window_size, status, st_df):
-    windows = defaultdict(partial(deque, [0]*window_size, maxlen=window_size))
 
-    async with aiohttp.ClientSession(trust_env = True) as session:
-        print("1")
+async def consumer(status, st_df):
+    # WS_CONN = "ws://localhost:8000/retrive_data?interface=WiFi"
+    WS_CONN = "ws://localhost:8000/retrive_data"
+    async with aiohttp.ClientSession(trust_env=True) as session:
         status.subheader(f"Connecting to {WS_CONN}")
-        print("1 Connecting to: ", WS_CONN)
         async with session.ws_connect(WS_CONN) as websocket:
             print("1 Connected to done: ", WS_CONN)
             status.subheader(f"Connected to: {WS_CONN}")
             print(websocket)
-            async for message in websocket:
-                print("1 Message: ", message)
-                data = message.json()
-                print("1 Data: ", data)
+            # data = {"channel": "A", "data": [1, 2, 3]}
+            # await websocket.send_json(data)
 
+            async for message in websocket:
+                # print("1 Message: ", message)
+                data = message.json()
                 try:
                     # id data is empty
                     if not data:
@@ -35,7 +34,7 @@ async def consumer(graphs, selected_channels, window_size, status, st_df):
                         # show in a st.dataframe
                         pass
                     # show st.dataframe with random data
-                    random_df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
+                    # random_df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
                     # st_df.dataframe(random_df)
                     data_json = json.loads(data)
 
@@ -45,9 +44,33 @@ async def consumer(graphs, selected_channels, window_size, status, st_df):
 
                     # print(type(data))
                     df = pd.DataFrame(data_json)
-                    print(df)
+                    # print(df)
+                    # get unique interfaces from the dataframe
+                    interfaces = df["interface"].unique()
+                    # multiselect
+                    # create a place holder to keep a set of
+                    # selected_interfaces = st_df.multiselect("Select interfaces", interfaces, default=interfaces[0])
+                    # drop down menu
+                    selected_dropdown = st.selectbox("Select interfaces sss", interfaces)
+                    print("Selected interface Selected dropdown: ", selected_dropdown)
+                    # get the index of the selected interface
+                    index = interfaces.index(selected_dropdown)
+                    print("Selected interface Index: ", index)
+                    # get the selected interface
+                    selected_interface = interfaces[index]
+                    print("Selected interface: ", selected_interface)
+                    st.write(selected_interface)
+                    # for interface in interfaces:
+                    st_placeholder = st.empty()
+                    # if interface not in interfaces:
+                    #     continue
+                    # filter dataframe by interface
+                    df_inf = df[df["interface"] == selected_interface]
+                    # remove interface column
+                    df_inf = df_inf.drop(columns=["interface"])
+                    # show dataframe
+                    st_df.dataframe(df_inf)
 
-                    st_df.dataframe(df)
                     status.subheader(f"Received: {len(df)} rows")
                 except Exception as e:
                     if e != "TypeError: string indices must be integers":
@@ -65,3 +88,47 @@ async def consumer(graphs, selected_channels, window_size, status, st_df):
                 #         graph.area_chart(channel_data)
                 #     elif channel == "C":
                 #         graph.bar_chart(channel_data)
+
+
+async def consumer_one(status, st_df, interface):
+    # windows = defaultdict(partial(deque, [0] * window_size, maxlen=window_size))
+    WS_CONN = "ws://localhost:8000/retrive_data?interface=" + interface
+    print("consumer_one WS_CONN: ", WS_CONN)
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        status.subheader(f"consumer_one Connecting to {WS_CONN}")
+        st.text("consumer_one Connecting to: " + WS_CONN)
+        print("consumer_one Connecting to: ", WS_CONN)
+        async with session.ws_connect(WS_CONN) as websocket:
+            # send interface to server
+            print("consumer_one Connected to done: ", WS_CONN)
+            status.subheader(f"consumer_one Connected to: {WS_CONN}")
+            print(websocket)
+            # data = {"channel": "A", "data": [1, 2, 3]}
+            # await websocket.send_json(data)
+            print("before consumer_one Sent interface to server ", interface, websocket)
+            # await websocket.send_json({"interface": interface})
+            print("consumer_one Sent interface to server", interface, websocket)
+            async for message in websocket:
+                print("consumer_one Message: ", message)
+                data = message.json()
+                try:
+                    if not data:
+                        print("No data")
+                    else:
+                        pass
+                    data_json = json.loads(data)
+
+                    # check if json is in the right format
+                    if not isinstance(data_json, list):
+                        raise Exception("Data retrieved is not in JSON format")
+
+                    # print(type(data))
+                    df = pd.DataFrame(data_json)
+
+                    st_df.dataframe(df)
+                    status.subheader(f"Received: {len(df)} rows")
+                except Exception as e:
+                    if e != "TypeError: string indices must be integers":
+                        print(e)
+                    else:
+                        print("1 ")
--- utils_anom.py
+++ utils_anom.py
@@ -1,4 +1,6 @@
+import asyncio
 import json
+import loguru
 
 import aiohttp
 from collections import deque, defaultdict
@@ -9,56 +11,75 @@ import pandas as pd
 import streamlit
 import streamlit as st
 
-WS_CONN = "ws://localhost:8000/anomalities"
-async def consumer_anom(graphs, selected_channels, window_size, status, st_df):
-    windows = defaultdict(partial(deque, [0]*window_size, maxlen=window_size))
+from utils import consumer, consumer_one
 
-    async with aiohttp.ClientSession(trust_env = True) as session:
-        print("2")
+
+async def consumer_anom(graphs, selected_channels, window_size, status, st_df):
+    WS_CONN = "ws://localhost:8000/anomalities"
+    windows = defaultdict(partial(deque, [0] * window_size, maxlen=window_size))
+    async with aiohttp.ClientSession(trust_env=True) as session:
         status.subheader(f"Connecting to {WS_CONN}")
-        print("2 Connecting to: ", WS_CONN)
         async with session.ws_connect(WS_CONN) as websocket:
             status.subheader(f"Connected to: {WS_CONN}")
             print("2 Connected to: ", WS_CONN)
             async for message in websocket:
                 data = message.json()
-                print("2 Data: ", data)
                 try:
                     # id data is empty
                     if not data:
                         print("No data")
                     else:
-                        print("Data: ", data)
-                        # show in a st.dataframe
+                        st_df.write(data)
                         pass
-                    # # show st.dataframe with random data
-                    # random_df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
-                    # # st_df.dataframe(random_df)
-                    # data_json = json.loads(data)
-                    #
-                    # # check if json is in the right format
-                    # if not isinstance(data_json, list):
-                    #     raise Exception("Data retrieved is not in JSON format")
-                    #
-                    # # print(type(data))
-                    # df = pd.DataFrame(data_json)
-                    # print(df)
-                    #
-                    # st_df.dataframe(df)
-                    # status.subheader(f"Received: {len(df)} rows")
+
                 except Exception as e:
                     if e != "TypeError: string indices must be integers":
                         print(e)
                     else:
                         print("1 ")
 
-                # windows[data["channel"]].append(data["data"])
-                #
-                # for channel, graph in graphs.items():
-                #     channel_data = {channel: windows[channel]}
-                #     if channel == "A":
-                #         graph.line_chart(channel_data)
-                #     elif channel == "B":
-                #         graph.area_chart(channel_data)
-                #     elif channel == "C":
-                #         graph.bar_chart(channel_data)
+
+async def consumer_interfaces(status, st_df):
+    WS_CONN = "ws://localhost:8000/interfaces"
+    # windows = defaultdict(partial(deque, [0]*window_size, maxlen=window_size))
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        status.subheader(f"Connecting to {WS_CONN}")
+        async with session.ws_connect(WS_CONN) as websocket:
+            status.subheader(f"Connected to: {WS_CONN}")
+            print("3 Connected to: ", WS_CONN)
+            print(websocket)
+            async for message in websocket:
+                print("3 Message: ", message)
+                data = message.json()
+                try:
+                    if not data:
+                        print("No data")
+                    else:
+                        print("3 Data: ", data)
+                        selected_interfaces = st_df.multiselect("Select interfaces", data, default=data[0])
+                        # drop down menu
+                        selected_dropdown = st.selectbox("Select interfaces", data)
+                        # get the index of the selected interface
+                        index = data.index(selected_dropdown)
+                        # get the selected interface
+                        selected_interface = data[index]
+                        # Update session state when a new item is selected
+                        if selected_dropdown != st.session_state.selected_interface:
+                            st.session_state.selected_interface = selected_dropdown
+
+                        # add selected interfaces to the st.session_state
+                        st.session_state.selected_interfaces = selected_interfaces
+                        st.write("Selected interfaces: ", selected_interfaces)
+                        st.write("Selected interfaces: ", st.session_state.selected_interfaces)
+                        columns = [col.empty() for col in st.columns(len(selected_interfaces))]
+                        print(st.session_state.selected_interfaces)
+                        button = st.button("Start")
+
+
+
+
+                except Exception as e:
+                    if e != "TypeError: string indices must be integers":
+                        print(e)
+                    else:
+                        print("1 ")
